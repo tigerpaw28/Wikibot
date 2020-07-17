b46d22b03c8a7ed4032f52e139c55bcf6fd07dc8
Tigerpaw28 <Tigerpaw28@hotmail.com>
2020-07-17 16:47:25 -0400

    Timestamp setting

--- Wikibot.App/Models/Jobs/TestJob.cs
+++ Wikibot.App/Models/Jobs/TestJob.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentScheduler;
+using Microsoft.Extensions.Configuration;
 
 namespace Wikibot.App.Jobs
 {
     public class TestJob : AbstractJob
     {
+        public TestJob(IConfiguration config) {
+            Configuration = config;
+        }
+
         public void Execute() {
-            Console.WriteLine("Test Job writing to console.");
+
+            SetJobStart();
+            try
+            {
+                Console.WriteLine("Test Job writing to console.");
+            }
+            catch(Exception ex)
+            {
+                Comment = $"An error occurred: {ex.StackTrace}";
+            }
+            finally
+            {
+                SetJobEnd();
+                SaveJob();
+            }
         }
     }
 }
