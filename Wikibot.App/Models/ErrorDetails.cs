90167f543c552498ed4f72458e4a0012463eb8bd
Tigerpaw28 <Tigerpaw28@hotmail.com>
2020-10-15 16:48:12 -0400

    Missing error handling middleware extension implementation

--- Wikibot.App/Models/ErrorDetails.cs
+++ Wikibot.App/Models/ErrorDetails.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,11 @@ namespace Wikibot.App.Models
 {
     public class ErrorDetails
     {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
     }
 }
