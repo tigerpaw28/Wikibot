61e2f3b651ab13dacfe6300b7ba0da84a86bee57
Tigerpaw28 <Tigerpaw28@hotmail.com>
2020-11-18 14:16:40 -0500

    Adding hack to keep app alive in IIS when app pool is not configurable

--- Wikibot.Jobs/Jobs/JobRetrievalJob.cs
+++ Wikibot.Jobs/Jobs/JobRetrievalJob.cs
@@ -106,7 +106,11 @@ namespace Wikibot.Logic.Jobs
 
 
                 } 
-
+                string keepAliveURL = Configuration["KeepAliveURL"];
+                if (keepAliveURL != string.Empty)
+                {
+                    HttpGet(keepAliveURL); //hack to keep IIS server alive
+                }
             }
             catch(Exception ex)
             {
@@ -129,5 +133,16 @@ namespace Wikibot.Logic.Jobs
             }
         }
 
+        private void HttpGet(string serverURI)
+        {
+            WebClient client = new WebClient();
+
+            // Add a user agent header in case the 
+            // requested URI contains a query.
+
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+            Stream data = client.OpenRead(serverURI);
+        }
     }
 }
