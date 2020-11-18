89ef0537a2485ca3e6162a8e83d44db90a760d53
Tigerpaw28 <Tigerpaw28@hotmail.com>
2020-11-18 14:04:03 -0500

    ApplicationDb updates

--- Wikibot.App/Migrations/20201114031520_applicationdbupdate.Designer.cs
+++ Wikibot.App/Migrations/20201114031520_applicationdbupdate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Wikibot.App.Data;
 
 namespace Wikibot.App.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201114031520_applicationdbupdate")]
+    partial class applicationdbupdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
