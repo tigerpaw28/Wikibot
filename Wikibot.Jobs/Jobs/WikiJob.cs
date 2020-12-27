84bbe8da6410e3df4f86b2b641b5644aeeb439f1
Tigerpaw28 <Tigerpaw28@hotmail.com>
2020-12-27 16:13:00 -0500

    Bugfixes: Updating request in SaveRequest and return correct To and From text

--- Wikibot.Jobs/Jobs/WikiJob.cs
+++ Wikibot.Jobs/Jobs/WikiJob.cs
@@ -2,9 +2,11 @@
 using MwParserFromScratch;
 using MwParserFromScratch.Nodes;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Wikibot.DataAccess;
 using Wikibot.DataAccess.Objects;
+using Wikibot.Logic.JobRetrievers;
 
 namespace Wikibot.Logic.Jobs
 {
@@ -28,7 +30,7 @@ namespace Wikibot.Logic.Jobs
                 if (_fromText == null)
                 {
                     var template = new WikitextParser().Parse(Request.RawRequest).Lines.SelectMany(x => x.EnumDescendants().OfType<Template>()).Single();
-                    _fromText =  template.Arguments.Single(arg => arg.Name.ToPlainText() == "before").Value.ToPlainText();
+                    _fromText =  template.Arguments.Single(arg => arg.Name.ToPlainText() == "before").Value.ToString();
                 }
                 return _fromText;
             }
@@ -42,12 +44,17 @@ namespace Wikibot.Logic.Jobs
                 if (_toText == null)
                 {
                     var template = new WikitextParser().Parse(Request.RawRequest).Lines.SelectMany(x => x.EnumDescendants().OfType<Template>()).Single();
-                    _toText = template.Arguments.Single(arg => arg.Name.ToPlainText() == "after").Value.ToPlainText();
+                    _toText = template.Arguments.Single(arg => arg.Name.ToPlainText() == "after").Value.ToString();
                 }
                 return _toText;
             } 
             
         }
+
+        //public WikiJob(IWikiJobRetriever jobRetriever, IConfigurationSection config) {
+        //    Retriever = jobRetriever;
+        //    Configuration = config;
+        //}
 
         public void SetJobStart()
         {
@@ -85,6 +92,9 @@ namespace Wikibot.Logic.Jobs
             Log.Information("Saving job.");
 
             JobData.UpdateStatus(this.Request.ID, this.Request.Status);
+            Retriever.UpdateRequests(new List<WikiJobRequest> { Request });
         }
+
+        public IWikiJobRetriever Retriever;
     }
 }
