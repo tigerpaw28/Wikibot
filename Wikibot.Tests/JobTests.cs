eab51613c30b14b965b642378b4b6f427c869226
Tigerpaw28 <Tigerpaw28@hotmail.com>
2021-06-10 02:21:01 -0400

    Adding exceptions to debug where CI is hanging

--- Wikibot.Tests/JobTests.cs
+++ Wikibot.Tests/JobTests.cs
@@ -19,6 +19,7 @@ namespace Wikibot.Tests
         [Fact]
         public void ExecuteTextReplacementJob()
         {
+            throw new System.Exception("ExecuteTextReplacementJob");
             var iConfig = Utilities.GetIConfigurationRoot();
             var wikiAccessLogic = new WikiAccessLogic();
             var log = Utilities.GetLogger(iConfig, _output);
@@ -34,6 +35,7 @@ namespace Wikibot.Tests
         [Fact]
         public void ExecuteLinkFixJob()
         {
+            throw new System.Exception("ExecuteLinkFixJob");
             var iConfig = Utilities.GetIConfigurationRoot();
             var wikiAccessLogic = new WikiAccessLogic();
             var log = Utilities.GetLogger(iConfig, _output);
@@ -49,6 +51,7 @@ namespace Wikibot.Tests
         [Fact]
         public void ExecuteLinkFixJobLinkTextNotRetainedIfNoCustomText()
         {
+            throw new System.Exception("ExecuteLinkFixJob2");
             var iConfig = Utilities.GetIConfigurationRoot();
             var wikiAccessLogic = new WikiAccessLogic();
             var log = Utilities.GetLogger(iConfig, _output);
@@ -64,6 +67,7 @@ namespace Wikibot.Tests
         [Fact]
         public void ExecuteContinuityLinkFixJob()
         {
+            throw new System.Exception("ExecuteContinuityLinkFixJob");
             var iConfig = Utilities.GetIConfigurationRoot();
             var wikiAccessLogic = new WikiAccessLogic();
             var log = Utilities.GetLogger(iConfig, _output);
