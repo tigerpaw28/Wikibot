c92c9c70f0abf8821a696432eac260974c42084b
Tigerpaw28 <Tigerpaw28@hotmail.com>
2021-06-10 15:01:50 -0400

    Checking to see if this test hangs in CI

--- Wikibot.Tests/JobTests.cs
+++ Wikibot.Tests/JobTests.cs
@@ -35,6 +35,7 @@ namespace Wikibot.Tests
         [Fact]
         public void ExecuteLinkFixJob()
         {
+            throw new System.Exception("ExecuteLinkFixJob");
             var iConfig = Utilities.GetIConfigurationRoot();
             var wikiAccessLogic = new WikiAccessLogic();
             var log = Utilities.GetLogger(iConfig, _output);
@@ -50,7 +51,6 @@ namespace Wikibot.Tests
         [Fact]
         public void ExecuteLinkFixJobLinkTextNotRetainedIfNoCustomText()
         {
-            throw new System.Exception("ExecuteLinkFixJob2");
             var iConfig = Utilities.GetIConfigurationRoot();
             var wikiAccessLogic = new WikiAccessLogic();
             var log = Utilities.GetLogger(iConfig, _output);
