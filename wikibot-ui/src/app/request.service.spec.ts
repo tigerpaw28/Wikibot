9fac6eeba6029cb790e34e6a45d578a0cae6798b
Tigerpaw28 <Tigerpaw28@hotmail.com>
2022-10-15 01:07:34 -0400

    Trying this to see if it resolves a CI error where it can't hit the real service

--- wikibot-ui/src/app/request.service.spec.ts
+++ wikibot-ui/src/app/request.service.spec.ts
@@ -1,16 +1,17 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { MockRequestService } from './mock-request-service';
 
 import { RequestService } from './request.service';
 
 describe('RequestService', () => {
-  let service: RequestService;
+  let service: MockRequestService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule]
     });
-    service = TestBed.inject(RequestService);
+    service = TestBed.inject(MockRequestService);
   });
 
   it('should be created', () => {
