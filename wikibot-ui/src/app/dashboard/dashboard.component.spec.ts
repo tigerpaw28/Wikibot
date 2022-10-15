27fc1ee2db5a5541cc38d22efd0278d5ee7432d5
Tigerpaw28 <Tigerpaw28@hotmail.com>
2022-10-15 01:26:51 -0400

    Adding provider to set mock service in hopes of locating origin of CI npm test error

--- wikibot-ui/src/app/dashboard/dashboard.component.spec.ts
+++ wikibot-ui/src/app/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppRoutingModule } from '../app-routing.module';
+import { MockRequestService } from '../mock-request-service';
+import { RequestService } from '../request.service';
 import { ResultsTableComponent } from '../results-table/results-table.component';
 
 import { DashboardComponent } from './dashboard.component';
@@ -13,7 +15,9 @@ describe('DashboardComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ DashboardComponent, ResultsTableComponent ],
       imports: [ AppRoutingModule],
-      providers: [ResultsTableComponent, HttpClient, HttpHandler ]
+      providers: [ResultsTableComponent, HttpClient, HttpHandler,
+        { provides: RequestService, useClass: MockRequestService }
+      ]
     })
     .compileComponents();
   });
